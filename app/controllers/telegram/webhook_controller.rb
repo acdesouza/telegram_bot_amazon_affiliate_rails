c0efe329a2982d19c269527c0f62dbf1675e5936
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2022-11-21 12:24:33 -0300

    Only answers to message, the bot can read, with amazon links send by humans #TheHardWay.

--- app/controllers/telegram/webhook_controller.rb
+++ app/controllers/telegram/webhook_controller.rb
@@ -3,19 +3,17 @@ class Telegram::WebhookController < ApplicationController
     puts "-" * 50
     pp params
 
-    chat_id = params[:message][:chat][:id]
-    sender  = params[:message][:from][:first_name]
-    msg_id  = params[:message][:message_id]
-    msg     = params[:message][:text]
-    answer  = "Hello, #{sender}!\n Did you said: '#{msg}'?"
+    chat_id = params.dig(:message, :chat, :id)
+    sender  = params.dig(:message, :from, :first_name)
+    is_bot  = params.dig(:message, :from, :is_bot)
+    msg_id  = params.dig(:message, :message_id)
+    msg     = params.dig(:message, :text)
 
-    message(chat_id: chat_id,
-            reply_to_message_id: msg_id,
-            text: answer)
-
-    puts '.' * 50
-    pp answer
-    puts "-" * 50
+    if should_answer?(text: msg, is_bot: is_bot)
+      message(chat_id: chat_id,
+              reply_to_message_id: msg_id,
+              text: answer(sender: sender, text: msg))
+    end
   end
 
   private
@@ -33,5 +31,28 @@ class Telegram::WebhookController < ApplicationController
     puts "_" * 50
     puts res.body
     puts "_" * 50
+  end
+
+  def answer(sender:, text:)
+    answer  = "Hello, #{sender}!\n Did you said: '#{text}'?"
+
+    puts '.' * 50
+    pp answer
+    puts "-" * 50
+
+    answer
+  end
+
+  def should_answer?(text:, is_bot:)
+    return text.present? &&
+           !is_bot &&
+           has_amazon_url?(text)
+  end
+
+  def has_amazon_url?(text)
+    extracted_urls = URI.extract(text)
+    extracted_urls.any? do |url|
+      URI(url).hostname.match?(/amazon.com.br/)
+    end
   end
 end
